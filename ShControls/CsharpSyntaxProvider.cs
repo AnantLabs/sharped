1a277d1d4774e14e2abbb74be93d8635c0e773fc
astro64m <astro64m@localhost>
2009-10-13 21:12:25 +0400

    single line comment definition

---
+++ ShControls/CsharpSyntaxProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ShControls
+{
+    internal struct TokenDefinition
+    {
+        internal TokenDefinition(string regexp, FontWeight fontWeight, Color foregroundColor)
+        {
+            Regexp = regexp;
+            FontWeight = fontWeight;
+            ForegroundColor = foregroundColor;
+        }
+        public string Regexp;
+        public FontWeight FontWeight;
+        public Color ForegroundColor;
+    }
+
+    class CsharpSyntaxProvider
+    {
+        public static TokenDefinition[] Definitions = 
+        {
+            // quoted string
+            new TokenDefinition("\".*?\"", FontWeights.Normal, Colors.DarkRed),
+            // comment
+            new TokenDefinition("//.*", FontWeights.Normal, Colors.Green)
+        };
+    }
+}
